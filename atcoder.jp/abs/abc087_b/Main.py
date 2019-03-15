51572d47706b8941157d5eec55a038d459e5de93
Nawta <wes904@icloud.com>
2019-03-15 12:24:24 +0900

    [AC] abs abc087_b

---
+++ atcoder.jp/abs/abc087_b/Main.py
@@ -0,0 +1,10 @@
+a, b, c, x = list(map(int, [input() for i in range(4)]))
+count=0
+
+for i in range(a+1):
+  for j in range(b+1):
+    for k in range(c+1):
+      if i*500 + j*100 + k*50 == x:
+        count +=1
+        
+print(count)
