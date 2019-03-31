96d842e5393b1105f1dc9083d1732a8e5c338a96
Nawta <wes904@icloud.com>
2019-03-31 14:44:05 +0900

    [AC] abc011 abc011_1

---
+++ atcoder.jp/abc011/abc011_1/Main.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <string> 
+
+using namespace std;
+int main(){
+  int a;
+  cin >> a;
+  if(a != 12){
+  cout << a+1 << endl;
+}else{
+  cout << 1 << endl;
+}
+  return 0;
+}
