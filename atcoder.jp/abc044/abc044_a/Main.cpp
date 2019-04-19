604075c378ee800218792e2e7209535dead14fc3
Nawta <wes904@icloud.com>
2019-04-19 22:50:26 +0900

    [AC] abc044 abc044_a

---
+++ atcoder.jp/abc044/abc044_a/Main.cpp
@@ -0,0 +1,19 @@
+#include <bits/stdc++.h>
+#define ll long long
+#define INIT std::ios::sync_with_stdio(false);std::cin.tie(0);
+#define FOR(i,a,b) for(int i=(a);i<(b);++i)
+#define REP(i,n)  FOR(i,0,n)
+#define dump(x)  cerr << #x << " = " << (x) << endl;
+#define debug(x) cerr << #x << " = " << (x) << " (L" << __LINE__ << ")" << " " << __FILE__ << endl;
+
+using namespace std;
+
+int main() {
+  INIT;
+  
+  int n,k,x,y;
+  cin >> n >> k >> x >> y;
+  cout << (n <= k)*(x*n) + (n > k)*( x*k + (n-k)*y ) << endl;
+  
+  return 0;
+}
