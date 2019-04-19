cf8abe9d4007ef1495eb52f74a7caf7e015e3ddb
Nawta <wes904@icloud.com>
2019-04-19 21:57:47 +0900

    [AC] abc039 abc039_a

---
+++ atcoder.jp/abc039/abc039_a/Main.cpp
@@ -0,0 +1,19 @@
+#include <bits/stdc++.h>
+#define ll long long
+#define INIT std::ios::sync_with_stdio(false);std::cin.tie(0);
+#define FOR(i,a,b) for(int i=(a);i<(b);++i)
+#define REP(i,n)  FOR(i,0,n)
+#define dump(x)  cerr << #x << " = " << (x) << endl;
+#define debug(x) cerr << #x << " = " << (x) << " (L" << __LINE__ << ")" << " " << __FILE__ << endl;
+
+using namespace std;
+int main() {
+  INIT;
+  
+  int a,b,c ;
+  cin >> a >> b >> c;
+
+  cout << 2*(a*b + b*c + c*a) << endl;
+  
+  return 0;
+}
