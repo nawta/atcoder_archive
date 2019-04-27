a584d0474d1216dd73350bee68b27493b43f1bd1
Nawta <wes904@icloud.com>
2019-04-28 08:48:44 +0900

    [AC] abc099 abc099_a

---
+++ atcoder.jp/abc099/abc099_a/Main.cpp
@@ -0,0 +1,22 @@
+#include <bits/stdc++.h>
+#define ll long long
+#define INIT std::ios::sync_with_stdio(false);std::cin.tie(0);
+#define FOR(i,a,b) for(int i=(a);i<(b);++i)
+#define REP(i,n)  FOR(i,0,n)
+#define dump(x)  cerr << #x << " = " << (x) << endl;
+#define debug(x) cerr << #x << " = " << (x) << " (L" << __LINE__ << ")" << " " << __FILE__ << endl;
+
+using namespace std;
+
+int main() {
+  INIT;
+  
+  int n;
+  cin>> n;
+  if(n >=1000){
+  cout << "ABD"<< endl;
+}else{
+  cout << "ABC"<< endl;
+}
+  return 0;
+}
