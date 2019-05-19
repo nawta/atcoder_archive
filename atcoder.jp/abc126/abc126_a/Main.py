4bb5c3eece9045eecacb68403ab8b295a3eed894
Nawta <wes904@icloud.com>
2019-05-19 21:19:21 +0900

    [AC] abc126 abc126_a

---
+++ atcoder.jp/abc126/abc126_a/Main.py
@@ -0,0 +1,12 @@
+import sys
+n,k = list(map(int, input().split()))
+s = list(input())
+
+if s[k-1] == "A":
+    s[k-1] = "a"
+elif s[k-1] == "B":
+    s[k-1] = "b"
+else:
+    s[k-1] = "c"
+
+print("".join(s))
