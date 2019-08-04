b94c39863f4be109672a5d42d86f752dd164528d
Nawta <wes904@icloud.com>
2019-08-04 21:17:50 +0900

    [AC] abc136 abc136_b

---
+++ atcoder.jp/abc136/abc136_b/Main.py
@@ -0,0 +1,16 @@
+n = input()
+keta = len(n)
+n = int(n)
+
+if keta == 1:
+    print(n)
+elif keta == 2:
+    print(9)
+elif keta == 3:
+    print(n - 90)
+elif keta == 4:
+    print(909)
+elif keta == 5:
+    print(n - 9000  - 90 )
+else:
+    print(90909)
